34cf0157bce212c9a50daa06cd62a5b231190d8d
TEDTEDTEDTEDTED <lf2netted@gmail.com>
2017-10-31 11:59:52 +0800

    innitial commit

---
+++ app/src/main/java/com/tedtedtedtedted/bmi6/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tedtedtedtedted.bmi6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+         EditText edweight =(EditText) findViewById(R.id.ed_weight);
+        EditText edheight =(EditText) findViewById(R.id.ed_height);
+        float weight = Float.parseFloat(edweight.getText()).toString.();
+        float weight = Float.parseFloat(edweight.getText(edweight);
+
+
+    }
+}
